ddbd72aa9304af9c0c31b52fab574143f0a50766
sudaidai <brucepipi@gmail.com>
2020-06-15 16:39:50 +0800

    111

---
+++ app.js
@@ -0,0 +1,34 @@
+const SocketServer = require('websocket').server;
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {});
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
+
+wsServer = new SocketServer({httpServer:server});
+
+const connections = [];
+
+wsServer.on('request', (req) => {
+    const connection = req.accept();
+    console.log('new connection');
+    connections.push(connection);
+
+    connection.on('message', (mes) => {
+        connections.forEach(element => {
+            if(element != connection){
+            	console.log("connection: ", element.remoteAddress);
+            	console.log("message: ", mes);
+                element.sendUTF(mes.utf8Data);
+            }
+        });
+    });
+
+    connection.on('close', (resCode, des) => {
+        console.log('connection closed');
+        connections.splice(connections.indexOf(connection), 1);
+    });
+});
