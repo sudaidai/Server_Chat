ce1c87a002247df71bca716c8d6b89650a14527a
yanchen184 <bobchen184@gmail.com>
2020-06-19 15:05:55 +0800

    CCC
    CCC

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = 3000;
 
 var table = [];
-
+var tableNum = 0;
 
 const server = http.createServer((req, res) => { });
 
@@ -21,33 +21,62 @@ wsServer.on('request', (req) => {
     connections.push(connection);
 
     connection.on('message', (mes) => {
-
         console.log(JSON.parse(mes.utf8Data).kind);
         switch (JSON.parse(mes.utf8Data).kind) {
             case "room":
-                var roomString = "";
-                var roomNumber = Math.round((Math.random() * 10000));
-                if (roomNumber.toString().length < 4) {
-                    var diff = 4 - roomNumber.toString().length;
-                    for (i = 0; i < diff; i++) {
-                        roomString += 0;
+                do {
+                    /**生成文字 */
+                    roomString = "";
+                    var roomNumber = Math.round((Math.random() * 3));
+                    if (roomNumber.toString().length < 4) {
+                        var diff = 4 - roomNumber.toString().length;
+                        for (i = 0; i < diff; i++) {
+                            roomString += 0;
+                        }
                     }
-                }
-                console.log(roomString);
-                roomString += roomNumber;
+                    roomString += roomNumber;
+                    console.log(roomString);
+
+                    var boolean = false;
+                    table.forEach(element => {
+                        console.log("element : " + element.roomNumber);
+                        console.log("element : " + element);
+                        console.log(roomString);
+                        if (element.roomNumber == roomString) {
+                            boolean = true;
+                        }
+                    });
+
+                } while (boolean);
+                // table.push(roomString);
+
+
+                table[tableNum] = {};
+                table[tableNum].roomNumber = roomString;
+                table[tableNum].ip = [];
+                table[tableNum].ip.push(connection.remoteAddress);
+
+                tableNum = tableNum + 1;
+                console.log(table);
+
+
                 var vTableInfo;
                 vTableInfo =
                 {
                     type: 'utf8',
-                    utf8Data: `{"kind":"room","roomNum":${roomString}}`
+                    utf8Data: `{"kind":"room","roomNum":"${roomString}"}`
                 }
                 connections.forEach(element => {
                     if (element == connection) {
                         console.log("connection: ", element.remoteAddress);
-                        console.log("message: ", vTableInfo);
+                        console.log("room: ", vTableInfo);
                         element.sendUTF(vTableInfo.utf8Data);
                     }
                 });
+                // table.includes(roomString)
+
+
+                break;
             default:
                 connections.forEach(element => {
                     if (element != connection) {
@@ -61,6 +90,7 @@ wsServer.on('request', (req) => {
                         console.log("message: ", mes);
                     }
                 });
+                break;
         }
     });
 
